dddf5d14daa8fa16567add4d7f4f814053d48108
Ludovic Frank <ludovic.frank@icloud.com>
2018-11-25 19:03:46 +0100

    revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -691,7 +691,7 @@ class ApplicationController < ActionController::Base
         session[:destination_url] = destination_url
         # redirect_to path('/session/sso')
         # Really specific to Ideagora and Safari
-        redirect_to "https://compte.ideagora.fr/community/"
+        redirect_to "https://compte.ideagora.fr/"
       elsif params[:authComplete].present?
         redirect_to path("/login?authComplete=true")
       else
